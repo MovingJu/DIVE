7ba57f46124225faae6f85f2f9f98a5e9c59a769
nyhvic <nyhvic@gmail.com>
2025-08-24 01:21:42 +0900

    소셜로그인
    난모르겟따

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware  
 import routes, modules
 
 
 app = FastAPI()
-
+origins = [
+	"*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(routes.test.router)
--- routes/test.py
+++ routes/test.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter
 import aiomysql
 import modules
 import pandas as pd
+from dotenv import load_dotenv
+import os
+import requests
 
 router = APIRouter(
     prefix="/db",
@@ -29,15 +32,56 @@ async def class_test(table_name: str):
     return {"result": df.to_dict(orient="records")}
 
 
-@router.post("kakao/login/")
-async def kakaologin(name : str, email : str):
+@router.post("/kakao/authentication/")
+async def kakaologin(id : str):
     db = await modules.Manage.create()
     async with db.conn.cursor() as cursor:  
-        await cursor.execute('SELECT user_type FROM users WHERE name = %s AND email = %s',(name,email))
+        await cursor.execute('SELECT user_type FROM users WHERE id = %s',(id))
         result=await cursor.fetchall()
     df=pd.DataFrame(result,columns=['user_type'])
     await db.close()
     return {"result" : df.to_dict(orient='records')}
+    
+@router.post("/kakao/authcode")
+async def kakaoregister(authCode : str):
+    data = {
+        'grant_type': 'authorization_code',  
+        'client_id': os.getenv('KAKAO_API_KEY'),              
+        'redirect_uri': os.getenv('REDIRECT_URI'),            
+        'code': authCode     
+    }
+
+    # 카카오 인증 서버에 액세스 토큰 요청
+    resp = requests.post("https://kauth.kakao.com/oauth/token", data=data)
+    token=resp.json()['access_token']
+
+
+    headers = {
+        'Authorization': 'Bearer ' + token  
+    }
+
+    user = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers)
+    userinfo=user.json()
+    userid=userinfo['id']
+    username=userinfo['properties']['nickname']
+    useremail=userinfo['kakao_account']['email']
+          
+    db = await modules.Manage.create()
+    async with db.conn.cursor() as cursor:  
+        await cursor.execute('SELECT EXISTS(SELECT 1 FROM users WHERE id = %s) AS exist',(userid))
+        result=await cursor.fetchall()
+    await db.close()
+    if(result[0][0]):
+        pass
+    else:
+        db = await modules.Manage.create()
+        async with db.conn.cursor() as cursor:  
+            await cursor.execute("INSERT INTO users (id, name, email, user_type) VALUES (%s, %s, %s,0)",(userid,username,useremail))
+            result=await cursor.fetchall()
+        await db.close()
+    
+    return userid
+
     
     
     
